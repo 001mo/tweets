6e90815539de42a5168fe7e8f70aa0c5448d679a
001mo <ahmedmohamadkhalaf@gmail.com>
2022-12-13 14:41:52 +0200

    conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      brand: ' #1DA1F2',
+      ...colors
+    },
   },
-  plugins: [],
 }
