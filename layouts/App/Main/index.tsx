3535b736f058ca36bb0d21162137b6fe90dda9f2
001mo <ahmedmohamadkhalaf@gmail.com>
2023-01-01 19:40:36 +0200

    remove bg style testing class

--- layouts/App/Main/index.tsx
+++ layouts/App/Main/index.tsx
@@ -1,6 +1,6 @@
 export default function App({ children }) {
     return (
-        <main className="grow min-[500px]:max-[687px]:w-full min-[688px]:grow-2 shrink flex flex-col items-stretch min-[688px]:items-start bg-slate-900">
+        <main className="grow min-[500px]:max-[687px]:w-full min-[688px]:grow-2 shrink flex flex-col items-stretch min-[688px]:items-start">
             <div className="min-[688px]:w-[600px] min-[988px]:w-[920px] min-[1078px]:w-[990px]">
                 <div className="flex-auto flex flex-row items-stretch content-between w-full min-height-full">
                     {children}
