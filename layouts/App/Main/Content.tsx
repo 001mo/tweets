09b09d0a07acfbef0f3ac2b25757b49dbc809068
001mo <ahmedmohamadkhalaf@gmail.com>
2023-01-04 19:36:55 +0200

    fix main section content section width overflow

--- layouts/App/Main/Content.tsx
+++ layouts/App/Main/Content.tsx
@@ -1,6 +1,6 @@
 export default function App({ children }) {
     return (
-        <div className="wrapper w-full min-[600px]:max-w-[600px] z-10 border-x border-x-gray-700">
+        <div className="wrapper grow min-[600px]:max-w-[600px] z-10 border-x border-x-gray-700">
             { children }
         </div>
     )
