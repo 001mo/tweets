3188ce462c4ee82ed002568ef2111235563a6f46
001mo <ahmedmohamadkhalaf@gmail.com>
2023-01-18 22:02:35 +0200

    add sm header sliding functionality

--- apps/home/Header/Sm/index.tsx
+++ apps/home/Header/Sm/index.tsx
@@ -9,35 +9,35 @@ export default function Header() {
     const { width: scWidth } = useWindowSize();
     const { palette: { mode }} = useTheme();
 
-    
-    const [prevScroll, setPrevScroll] = useState({ pos: 0, direction: 'down' });
+    const [prevScroll, setPrevScroll] = useState(0);
     const [show, setShow] = useState(true);
 
-    const addScrollListener = () => {
-        document.addEventListener('scroll', (e) => {
-            if(window.scrollY > prevScroll.pos) {
-                setPrevScroll({ pos: window.scrollY, direction: 'down' });
-                setShow(false);
-                return;
-            }
-            setPrevScroll({ pos: window.scrollY, direction: 'up' });
-            setShow(true);
-        });
+    const handleScroll = (e) => { // (d) fix this function
+        const scrollPos = document.documentElement.scrollTop;
+        if(scrollPos > prevScroll) {
+            setPrevScroll(scrollPos);
+            setShow(false)
+            return;
+        }
+        setPrevScroll(scrollPos);
+        setShow(true)
     }
 
     useEffect(() => {
-        addScrollListener();
+        document.addEventListener('scroll', handleScroll);
+        return () => document.removeEventListener("scroll", handleScroll);
     }, [])
 
     return scWidth < 500 ? (
-        // <Slide in={true} direction="down">
-            <Wrapper sx={{
-                bgcolor: mode === 'light' ? 'rgba(255, 255, 255, 0.85)' : 'rgba(0, 0, 0, 0.65)',
-                backdropFilter: 'blur(12px)',
-            }}>
-                <UpperRow />
-                <LowerRow />
-            </Wrapper>
-        // {/* </Slide> */}
+        <Wrapper sx={{
+            transitionDuration: '350ms',
+            transitionTimingFunction: 'cubic-bezier(0, 0, 0, 1)',
+            transform: show ? 'translateY(0)' : 'translateY(-53px)',
+            bgcolor: mode === 'light' ? 'rgba(255, 255, 255, 0.85)' : 'rgba(0, 0, 0, 0.65)',
+            backdropFilter: 'blur(12px)',
+        }}>
+            <UpperRow />
+            <LowerRow />
+        </Wrapper>
     ) : <></>;
 }
